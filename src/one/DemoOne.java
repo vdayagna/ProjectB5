b3b56832fc0e73b2850631088c1f62791796b8c5
vdaya <vdaya@LAPTOP-MUSS9J49.attlocal.net>
2021-11-24 19:58:54 -0500

    add method added

--- src/one/DemoOne.java
+++ src/one/DemoOne.java
@@ -4,6 +4,14 @@ public class DemoOne {
 	
 	public static void main(String[] args) {
 		System.out.println("Demo One code...");
+		
 	}
+	
+		
+		public static void add(int i, int b) {
+			
+			System.out.println(i + b);
+			
+		}
 
 }
